bd36d9c9795400a7f7690dc68d4d3febe477ff63
John Tabash <jrtabash@yahoo.com>
2020-03-25 19:07:43 -0500

    Added 'generate' test cases. Made misc updates to run tests script.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,33 +5,61 @@ if [ "$1" != "" ]; then
     binary=$1
 fi
 
-solution=/tmp/solution.pzl
 status=0
 
-function run_test_cases() {
+function test_solve() {
     mode=$1
+
+    solution=/tmp/solution.pzl
 
     for puzzle in $(ls test_puzzles/)
     do
-        echo "${mode}: ${puzzle}"
+        echo "Test solve: ${mode} ${puzzle}"
         ${binary} solve -m ${mode} -p test_puzzles/${puzzle} -f -s ${solution} > /dev/null
 
         ${binary} check -p ${solution} > /dev/null
         if [ $? -ne 0 ]; then
-            echo "Check failed"
+            echo "Solve check failed"
             status=1
         fi
 
         cmp ${solution} test_solutions/${puzzle} > /dev/null 2>&1
         if [ $? -ne 0 ]; then
-            echo "Comparison failed"
+            echo "Solve comparison failed"
             status=1
         fi
     done
 }
 
-run_test_cases "recursive"
-run_test_cases "iterative"
+function test_generate() {
+    difficulty=$1
+    spaces=$2
+
+    puzzle=/tmp/puzzle.pzl
+
+    echo "Test generate: ${difficulty}"
+    ${binary} generate -d ${difficulty} -f -s ${puzzle} > /dev/null
+
+    ${binary} check -w -p ${puzzle} > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Generate check failed"
+        status=1
+    fi
+
+    count=$(cat ${puzzle} | sed y/\ /\\n/ | grep -c 0)
+    if [ ${count} -ne ${spaces} ]; then
+        echo "Generate space count failed"
+        status=1
+    fi
+}
+
+test_solve "recursive"
+test_solve "iterative"
+
+test_generate "easy" 15
+test_generate "medium" 30
+test_generate "hard" 45
+test_generate "expert" 60
 
 if [ ${status} -eq 0 ]; then
     echo "Success"
